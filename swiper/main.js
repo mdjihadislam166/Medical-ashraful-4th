aa39c9eb6e2e764b67dcdc550eceb7e180872844
jihad <mdjihadislam166@gmail.com>
2022-11-23 02:18:59 +0600

    first commit

---
+++ swiper/main.js
@@ -0,0 +1,52 @@
+// bvhxvjb//
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 10,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+  0: {
+      slidesPerView: 1,
+  },
+  768: {
+      slidesPerView: 2,
+  },
+  991: {
+      slidesPerView:3,
+  },
+  },
+  
+  });
+
+
+
+//   //
+
+// bvhxvjb//
+var swiper = new Swiper(".mySwiperauto", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+  0: {
+      slidesPerView: 1,
+  },
+  768: {
+      slidesPerView: 2,
+  },
+  991: {
+      slidesPerView:5,
+  },
+  },
+  
+  });
+  
